ca30e15dcfbff35ebab9ce91fba81ba2b7ffd688
LitenApe <sonvo95@outlook.com>
2021-03-25 17:49:01 +0100

    style: style overview table

--- src/components/calculations.svelte
+++ src/components/calculations.svelte
@@ -20,35 +20,36 @@
 <section>
   <h2>Calculations</h2>
 
-  <table>
+  <table id="overview">
+    <caption>Investment growth the next 40 years.</caption>
     <thead>
-      <th>Time (year)</th>
-      <th>Value</th>
+      <th>Time</th>
       <th>Return</th>
+      <th>Value</th>
     </thead>
     <tbody>
       <tr>
-        <td>10</td>
-        <td>{$progress[10].value}</td>
+        <td>10 years</td>
         <td>{$progress[10].return}</td>
+        <td>{$progress[10].value}</td>
       </tr>
 
       <tr>
-        <td>20</td>
-        <td>{$progress[20].value}</td>
+        <td>20 years</td>
         <td>{$progress[20].return}</td>
+        <td>{$progress[20].value}</td>
       </tr>
 
       <tr>
-        <td>30</td>
-        <td>{$progress[30].value}</td>
+        <td>30 years</td>
         <td>{$progress[30].return}</td>
+        <td>{$progress[30].value}</td>
       </tr>
 
       <tr>
-        <td>40</td>
-        <td>{$progress[40].value}</td>
+        <td>40 years</td>
         <td>{$progress[40].return}</td>
+        <td>{$progress[40].value}</td>
       </tr>
     </tbody>
   </table>
@@ -70,3 +71,55 @@
     </tbody>
   </table>
 </section>
+
+<style>
+  section {
+    width: 100%;
+  }
+
+  #overview {
+    display: block;
+    width: 100%;
+    padding: 0 1rem;
+
+    max-width: 500px;
+    margin: 0 auto;
+  }
+
+  #overview > caption {
+    display: block;
+  }
+
+  #overview > thead > th {
+    width: 1px;
+    height: 1px;
+    overflow: hidden;
+    position: absolute;
+  }
+
+  #overview > tbody {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  #overview > tbody > tr {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    border: 1px solid rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
+
+    padding: 0.25rem 1rem;
+  }
+
+  #overview > tbody > tr > td:nth-child(2) {
+    color: green;
+  }
+
+  #overview > tbody > tr > td:last-child {
+    font-weight: bold;
+  }
+</style>
